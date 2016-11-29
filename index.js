96e61e7d6205ad40d31a1fec4f7975911ded3ba8
Guraz <guilleminraymond@hotmail.fr>
2016-11-29 12:07:41 +0100

    Revert "v0.2"
    This reverts commit e98849af841f33e2f77bfe5626ca328548fd3992.

--- index.js
+++ index.js
@@ -13,13 +13,10 @@ app.get('/', function (req, res) {
 });
 
 // Facebook Webhook
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === <VERIFY_TOKEN>) {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Invalid verify token');
+    }
 });
